4b8577fa664d9337250a605bef2489428311cd31
Venkadeshkissflow <85150759+Venkadeshkissflow@users.noreply.github.com>
2021-08-26 14:53:38 +0530

    Grid_Updated

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -5,37 +5,37 @@ export default function Grid(){
  return(
      <div className="container">
      <div className='OuterContainer'>
-         <div class="Right Rightnav">
+         <div className="Right Rightnav">
              side nav
          </div>
 
         <div className="center centerArea">
 
-         <div class="center1 centeralign1">
+         <div className="center1 centeralign1">
              First
          </div>
-                <div class="centersub1 centeralignsub1">
+                <div className="centersub1 centeralignsub1">
                     First sub1
                 </div>
-                <div class="centersub2 centeralignsub2">
+                <div className="centersub2 centeralignsub2">
                     First sub2
                 </div>
-                <div class="centersub3 centeralignsub3">
+                <div className="centersub3 centeralignsub3">
                     First sub3
                 </div>
-         <div class="center2 centeralign2">
+         <div className="center2 centeralign2">
              Second
          </div>
-         <div class="center31 centeralignsub31">
+         <div className="center31 centeralignsub31">
              Third
          </div>
-         <div class="center32 centeralignsub32">
+         <div className="center32 centeralignsub32">
              Third
          </div>
 
          </div>
 
-         <div class="left leftnav">
+         <div className="left leftnav">
              Left nav
          </div>
          </div>
